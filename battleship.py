913786bdea3fce486b23d5a162b7be99b92049bd
Jakub Wójtowicz <wojtjk@gmail.com>
2021-07-06 01:18:27 +0200

    Implemented ship detection

--- battleship.py
+++ battleship.py
@@ -3,13 +3,22 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 import plane
+from gamebutton import GameButton
 
 
 class Battleship(GridLayout):
+    isGameStarted = False
 
     def __init__(self, **kwargs):
         super(Battleship, self).__init__(**kwargs)
         self.ids['opponent'].disabled = True
+
+        for c1 in self.children:
+            for c2 in c1.children:
+                for c3 in c2.children:
+                    for c4 in c3.children:
+                        if isinstance(c4, GameButton):
+                            c4.sendMessage = self.sendMessage
 
     def startButtonClick(self):
         self.ids['player'].disabled = True
@@ -17,6 +26,25 @@ class Battleship(GridLayout):
         self.ids['startGameButton'].disabled = True
         self.ids['gameId'].disabled = True
         print("Click")
+        self.isGameStarted = True
+
+    def onMessage(self, message):
+        x = str(message['x'])
+        y = str(message['y'])
+        if self.isShip(x, y):
+            self.ids['opponent'].ids[y].ids[x].hit()
+        else:
+            self.ids['opponent'].ids[y].ids[x].miss()
+
+    def sendMessage(self, message):
+        if not self.isGameStarted:
+            return
+
+        print(message)
+        self.onMessage(message)
+
+    def isShip(self, x: str, y: str):
+        return self.ids['player'].ids[y].ids[x].isShip
 
 
 class BattleshipApp(App):
--- gamebutton.py
+++ gamebutton.py
@@ -1,19 +1,37 @@
-from kivy.properties import BooleanProperty, DictProperty
+from kivy.properties import BooleanProperty, DictProperty, ObjectProperty
 from kivy.uix.button import Button
 
 
 class GameButton(Button):
     coordinate = DictProperty({"x": 0, "y": 0})
     isShip = BooleanProperty(False)
+    wasHit = BooleanProperty(False)
+    sendMessage = ObjectProperty()
 
     def on_release(self):
         super(GameButton, self).on_release()
         self.isShip = not self.isShip
         self.updateColor()
-        print(self.coordinate)
+        self.sendMessage(self.coordinate)
+
+    def hit(self):
+        self.isShip = True
+        self.wasHit = True
+        print("Hit")
+        self.updateColor()
+
+    def miss(self):
+        self.isShip = False
+        self.wasHit = True
+        print("Miss")
+        self.updateColor()
 
     def updateColor(self):
-        if self.isShip:
+        if self.isShip and self.wasHit:
+            self.background_color = (0.9, 0, 0)
+        elif self.isShip and not self.wasHit:
             self.background_color = (0, 0.9, 0)
-        else:
+        elif not self.isShip and self.wasHit:
+            self.background_color = (0, 0, 0.9)
+        elif not self.isShip and not self.wasHit:
             self.background_color = (0.9, 0.9, 0.9)
