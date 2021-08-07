05d0dd48dc86385bce738bac1777b0c64d4ab0e8
Jakub Wójtowicz <wojtjk@gmail.com>
2021-08-07 12:23:49 +0200

    Implemented games cleanup

--- backend/game.py
+++ backend/game.py
@@ -1,16 +1,20 @@
 import asyncio
+import time
 
 
 class Game:
+    lastActivity = time.time()
     players = []
 
     def __init__(self):
         self.players = []
+        self.lastActivity = time.time()
 
     def add_player(self, websocket):
         self.players.append(websocket)
 
     async def handle(self, websocket, message):
+        self.lastActivity = time.time()
         if len(self.players) == 2:
             await self.sendToOther(websocket, message)
 
@@ -20,6 +24,10 @@ class Game:
                 continue
             await asyncio.sleep(0.5)
             await player.send(message)
+
+    async def timeout(self):
+        for player in self.players:
+            await player.send("Timeout")
 
     async def handleDisconnect(self, websocket):
         if self.players == 1:
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+import time
 
 import websockets
 
@@ -7,12 +8,31 @@ from backend.game import Game
 
 
 class Server:
+    clearInterval = 5
     games = {}  # Id, obiekt gry
     websocketToGame = {}  # websocket, ID Gry
 
     gameKeys = ['A', 'B', 'C']
 
-    async def echo(self, websocket, path):
+    async def clearOldGames(self):
+        while True:
+            size = len(self.games)
+            if size == 0:
+                print("No games to clear")
+                await asyncio.sleep(self.clearInterval)
+                continue
+            print("Starting cleanup")
+            for key in self.games.copy():
+                if time.time() - self.games[key].lastActivity > self.clearInterval:
+                    try:
+                        await self.games[key].timeout()
+                    except:
+                        print("Error during timeout notification")
+                    print("Game deleted", key)
+                    del self.games[key]
+                await asyncio.sleep(self.clearInterval / size)
+
+    async def handle(self, websocket, path):
         try:
             async for message in websocket:
                 if websocket not in self.websocketToGame:
@@ -25,20 +45,26 @@ class Server:
                         self.games[self.websocketToGame[websocket]] = Game()
                         self.games[self.websocketToGame[websocket]].add_player(websocket)
 
-                game = self.games[self.websocketToGame[websocket]]
-                await game.handle(websocket, message)
+                if self.websocketToGame[websocket] in self.games:
+                    game = self.games[self.websocketToGame[websocket]]
+                    await game.handle(websocket, message)
 
         except websockets.exceptions.ConnectionClosed:
-            game = self.games[self.websocketToGame[websocket]]
-            try:
-                await game.handleDisconnect(websocket)
-            except:
-                print("Trudno")
+            if self.websocketToGame[websocket] in self.games:
+                game = self.games[self.websocketToGame[websocket]]
+                try:
+                    await game.handleDisconnect(websocket)
+                except:
+                    print("Trudno")
             self.websocketToGame.pop(websocket)
 
 
 s = Server()
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(s.echo, 'localhost', 8765)
+    websockets.serve(s.handle, 'localhost', 8765)
 )
+asyncio.get_event_loop().run_until_complete(
+    s.clearOldGames()
+)
+
 asyncio.get_event_loop().run_forever()
