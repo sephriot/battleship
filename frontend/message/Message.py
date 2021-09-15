4a139cb0c714553d1044ec1154e31ef7e5ebc6cb
Jakub Wójtowicz <wojtjk@gmail.com>
2021-09-15 18:45:49 +0200

    29

---
+++ frontend/message/Message.py
@@ -0,0 +1,67 @@
+import json
+
+
+class BaseMessage:
+    NULL = "NULL"
+    PLAYER_DISCONNECTED = "PLAYER_DISCONNECTED"
+    PLAYER_CONNECTED = "PLAYER_CONNECTED"
+    ATTACK = "ATTACK"
+    HIT = "HIT"
+    MISS = "MISS"
+    SANK = "SANK"
+    GAME_ID_NOT_ALLOWED = "GAME_ID_NOT_ALLOWED"
+    YOU_WON = "YOU_WON"
+
+    def __init__(self, data=None):
+        self.type = self.NULL
+        if data is not None:
+            self.__dict__ = data
+
+    def __init_subclass__(cls, **kwargs):
+        BaseMessage.__init__(cls)
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+class PlayerDisconnectedMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.PLAYER_DISCONNECTED
+
+
+class PlayerConnectedMessage(BaseMessage):
+    def __init__(self, gameId):
+        self.type = self.PLAYER_CONNECTED
+        self.gameId = gameId
+
+
+class AttackMessage(BaseMessage):
+    def __init__(self, x=0, y=0):
+        self.type = self.ATTACK
+        self.x = x
+        self.y = y
+
+
+class HitMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.HIT
+
+
+class MissMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.MISS
+
+
+class SankMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.SANK
+
+
+class GameIDNowAllowedMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.GAME_ID_NOT_ALLOWED
+
+
+class YouWonMessage(BaseMessage):
+    def __init__(self):
+        self.type = self.YOU_WON
